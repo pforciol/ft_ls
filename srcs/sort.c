d37303afcb84ffd30c553dce7edb62e6da6c83e1
Pierre FORCIOLI <pforciol@student.42.fr>
2019-05-22 15:45:38 +0200

    :sparkles: Added -r option

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,13 +6,13 @@
 /*   By: pforciol <pforciol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:20:46 by pforciol          #+#    #+#             */
-/*   Updated: 2019/05/22 15:15:09 by pforciol         ###   ########.fr       */
+/*   Updated: 2019/05/22 15:41:15 by pforciol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 
-static void			sort_lexically(t_opt *opt, char *args[], int argc)
+static void			sort_lexically(t_opt *opt, char *args[], int argc, int r)
 {
 	int				i;
 	char			*tmp;
@@ -20,7 +20,14 @@ static void			sort_lexically(t_opt *opt, char *args[], int argc)
 	i = 0;
 	while (args[i] != NULL && i < (argc - 2 - opt->nb_opt))
 	{
-		if (ft_strcmp(args[i], args[i + 1]) > 0)
+		if (r == 0 && ft_strcmp(args[i], args[i + 1]) > 0)
+		{
+			tmp = args[i];
+			args[i] = args[i + 1];
+			args[i + 1] = tmp;
+			i = -1;
+		}
+		else if (r > 0 && ft_strcmp(args[i], args[i + 1]) < 0)
 		{
 			tmp = args[i];
 			args[i] = args[i + 1];
@@ -33,7 +40,9 @@ static void			sort_lexically(t_opt *opt, char *args[], int argc)
 
 char				**ls_sort(t_opt *opt, char *args[], int argc)
 {
-	if (opt->nb_opt == 0)
-		sort_lexically(opt, args, argc);
+	if (opt->nb_opt == 0 || opt->lr == 0)
+		sort_lexically(opt, args, argc, 0);
+	else if (opt->lr == 1 && opt->lt == 0)
+		sort_lexically(opt, args, argc, 1);
 	return (args);
 }
