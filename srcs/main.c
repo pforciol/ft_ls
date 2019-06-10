118003b51559b10d567bbb15bae3cf64ce59dac4
Pierre <pforciol@student.42.fr>
2019-06-10 16:59:39 +0200

    :sparkles: Changing way of initializing the list

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,32 @@
 /*   By: pforciol <pforciol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 13:41:06 by pforciol          #+#    #+#             */
-/*   Updated: 2019/05/29 11:43:44 by pforciol         ###   ########.fr       */
+/*   Updated: 2019/06/10 16:58:17 by pforciol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
+
+t_list				*ls_init_list(char **args, int nb_args, t_opt *opt)
+{
+	t_list			*l_args;
+	int				i;
+	
+	l_args = NULL;
+	if (nb_args == 0)
+		ft_getarg(".", l_args, opt);
+	else if (!(nb_args == opt->nb_opt + 1))
+	{
+		i = opt->nb_opt + 1;
+		while (args[i] != NULL && i < args)
+		{
+			l_args = ft_getarg(args[i], l_args, opt);
+			i++;
+		}
+		l_args = ls_sort(opt, l_args, nb_args);
+	}
+	return (l_args);
+}
 
 int					main(int argc, char *argv[])
 {
@@ -20,20 +41,7 @@ int					main(int argc, char *argv[])
 
 	opt = ft_get_opts(argv, argc);
 	l_args = NULL;
-	if (!(argc == opt->nb_opt + 1))
-	{
-		i = opt->nb_opt + 1;
-		while (argv[i] != NULL && i < argc)
-		{
-			l_args = ft_getarg(argv[i], l_args, opt);
-			i++;
-		}
-		l_args = ls_sort(opt, l_args, argc);
-	}
-	else if (argc == opt->nb_opt + 1)
-	{
-		l_args = ft_getarg(".", l_args, opt);
-	}
+	l_args = ls_init_list((char **)argv, argc - 1 - opt->nb_opt, opt);
 	debug(opt, argc, l_args);
 	return (0);
 }
